8c610da4e1f034dc806489730a8eccc41c514aad
Balazs Antal <39695760+Balazs89@users.noreply.github.com>
2023-02-06 20:38:40 +0100

    elso feladat elso metodus

---
+++ src/varnavizsga/jjbvizsga/feladat1/App.java
@@ -0,0 +1,14 @@
+package varnavizsga.jjbvizsga.feladat1;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+    System.out.println(Billing.priceWithVAT(27, 10000));
+    System.out.println(Billing.priceWithVAT(18, 11108));
+    System.out.println(Billing.priceWithVAT(5, 11111));
+	}
+
+}
--- src/varnavizsga/jjbvizsga/feladat1/Billing.java
+++ src/varnavizsga/jjbvizsga/feladat1/Billing.java
@@ -1,9 +1,14 @@
 package varnavizsga.jjbvizsga.feladat1;
 
 public class Billing {
+	
+	
 
     public static int priceWithVAT(int vatRate, int priceWithoutVAT) {
-        return -1;
+        return (int) Math.ceil(((1+vatRate/100.0)*priceWithoutVAT));
+        
+        
+      
     }
 
     public static int priceWithoutVAT(int vatRate, int priceWithVAT) {
